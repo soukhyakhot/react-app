26f8ad3c79d3cf30f9d2fd5067a17a1d82ea9485
Soukhya Khot <khotsoukhya@gmail.com>
2025-02-17 11:28:18 +0530

    enquirydetails

--- src/contactUs/Form.jsx
+++ src/contactUs/Form.jsx
@@ -1,6 +1,26 @@
 import styles from "./Form.module.css"
+import React, { useState } from "react";
+import axios from 'axios';
 
 function Form(){
+
+    const name = localStorage.getItem("username");
+    const email = localStorage.getItem("email");
+    const [mobileNo, setMobileNo] = useState('');
+    const [enquiry, setEnquiry] = useState('');
+
+
+    const handleEnquiry = () => {
+        // e.preventDefault();
+        const data = { Name: name, EmailId: email, MobileNo: mobileNo, EnquiryFor: enquiry };
+        axios.post("https://localhost:7066/api/Enquiry/enquiry", data)
+            .then((result) => {
+                alert("Thank you for your Enquiry! We will get back to you shortly!");
+            })
+            .catch((error) => {
+                alert("Enquiry request failed!" + error);
+            });
+    };
 
     return(
         <>
@@ -10,11 +30,11 @@ function Form(){
 
         <div className={styles.card}>
         <h2>Fill in Details for Enquiry</h2>
-            <input className={styles.name} type="text" placeholder="Name*"></input><br></br>
-            <input className={styles.telemail} type="tel" placeholder="Mobile No.*" ></input><br></br>
-            <input className={styles.telemail} type="email" placeholder="Email ID*" ></input><br></br>
-            <textarea className={styles.enquiry} placeholder="Enquiry For"></textarea>
-            <button className={styles.button}>Submit</button>
+            <input className={styles.name} type="text" placeholder="Name*" value={name} readOnly></input><br></br>
+            <input className={styles.telemail} type="tel" placeholder="Mobile No.*" pattern="[0-9]{10}" value={mobileNo} onChange={(e) => setMobileNo(e.target.value)} required></input><br></br>
+            <input className={styles.telemail} type="email" placeholder="Email ID*" value={email} readOnly></input><br></br>
+            <textarea className={styles.enquiry} placeholder="Enquiry For" value={enquiry} onChange={(e) => setEnquiry(e.target.value)} required></textarea>
+            <button className={styles.button} onClick={handleEnquiry}>Submit</button>
 
         </div>
         </>
